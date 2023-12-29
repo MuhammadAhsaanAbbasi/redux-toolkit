78482283d60da09b2c5e427edee1650881eb68b0
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2023-12-29 18:45:42 +0500

    placeholder

--- src/app/lib/todos/Addtodo.tsx
+++ src/app/lib/todos/Addtodo.tsx
@@ -5,23 +5,23 @@ import { addTodo } from '../features/todo/todoSlice'
 
 function AddTodo() {
 
-    const [input, setInput] = useState(" ")
+    const [input, setInput] = useState("")
 
     const dispatch = useDispatch()
     const addTodoHandler = (e:any) => {
         e.preventDefault()
         dispatch(addTodo(input))
-        setInput(" ")
+        setInput("")
     }
 
     return (
         <form onSubmit={addTodoHandler} className="space-x-3 mt-12 flex justify-center items-center">
-            <input
-                type="text"
-                className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                placeholder="Enter a Todo..."
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
+            <input 
+            type="text"
+            className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            placeholder='Enter Todo...'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             />
             <button
                 type="submit"
