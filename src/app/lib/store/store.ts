54b07e43af30f5222be1d2b21733b27ddb3fa877
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2023-12-29 11:47:36 +0500

    issue resolved

--- src/app/lib/store/store.ts
+++ src/app/lib/store/store.ts
@@ -1,16 +1,7 @@
 import todoReducer from '../features/todo/todoSlice';
 
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 
 export const Stored = configureStore({
         reducer: todoReducer
     })
-
-// const rootReducer = combineReducers({
-//     // Add other reducers here if you have more slices
-//     todos: todoReducer,
-// });
-
-// export type RootState = ReturnType<typeof rootReducer>
-
-// export type RootState = ReturnType<typeof Stored>;
